65d28a70b3ec5ff3a59f6d90e7d524e72f416b80
August Saint Freytag <me@augustfreytag.com>
2024-04-23 16:11:08 +0200

    Add config options for growth chance and min/max

--- src/main/java/net/saint/crop_growth_modifier/config/ModConfig.java
+++ src/main/java/net/saint/crop_growth_modifier/config/ModConfig.java
@@ -9,6 +9,15 @@ public class ModConfig implements ConfigData {
 	@Comment("Chance for a random world tick to be forwarded to a crop block (percentage). Default: 1.0 (100%)")
 	public float cropTickChance = 0.75f;
 
+	@Comment("Chance for an applied growth event to succeed and actually make the crop grow (e.g. bonemeal) (percentage). Default: 0.5 (50%)")
+	public float cropGrowthChance = 0.5f;
+
+	@Comment("Minimum amount of growth stages to apply to a crop block when applied, default is vanilla. Default: 2")
+	public int cropGrowthAmountMin = 2;
+
+	@Comment("Maximum amount of growth stages to apply to a crop block when applied, default is vanilla. Default: 5")
+	public int cropGrowthAmountMax = 5;
+
 	@Comment("Chance for an extra crop tick roll to happen. Extra rolls also follow the crop tick chance. Accelerates crop growth. Default: 0 (0%)")
 	public float cropExtraRollChance = 0f;
 
