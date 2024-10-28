b364b170f289c941b84e54adc09e5c075ba4ff42
August Saint Freytag <me@augustfreytag.com>
2024-10-28 17:25:26 +0100

    Let stem block mixin logic use stem multiplier

--- src/main/java/net/saint/crop_growth_modifier/mixinlogic/StemBlockMixinLogic.java
+++ src/main/java/net/saint/crop_growth_modifier/mixinlogic/StemBlockMixinLogic.java
@@ -1,4 +1,28 @@
 package net.saint.crop_growth_modifier.mixinlogic;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import net.saint.crop_growth_modifier.Mod;
+
 public interface StemBlockMixinLogic extends GrowableBlockMixinLogic {
+
+	public default boolean shouldAllowRandomTick(Block block, BlockState state, ServerWorld world, BlockPos pos,
+			Random random) {
+		var randomValue = random.nextFloat();
+
+		if (randomValue > Mod.config.cropTickChance * Mod.config.stemBlockMultiplier) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public default boolean shouldApplyGrowth(World world, BlockPos pos, BlockState state) {
+		return world.random.nextFloat() < Mod.config.cropTickChance * Mod.config.stemBlockMultiplier;
+	}
+
 }
